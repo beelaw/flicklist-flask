b55b94a3eea7d3de26f1fc8ac2ac317b6d612b55
beelaw <lawrenceleebrandon@gmail.com>
2017-12-19 20:38:32 -0500

    Finished rubrick

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # BeeLaw
 
 from flask import Flask, request
+import cgi
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+
+terrible_movies = ['Nine Lives', 'Love Actually']
 
 page_header = """
 <!DOCTYPE html>
@@ -14,6 +17,7 @@ page_header = """
     </head>
     <body>
         <h1>FlickList</h1>
+        {0}
 """
 
 page_footer = """
@@ -56,7 +60,7 @@ def crossoff_movie():
     crossed_off_movie = request.form['crossed-off-movie']
     crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
     confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
-    content = page_header + "<p>" + confirmation + "</p>" + page_footer
+    content = page_header.format('') + "<p>" + confirmation + "</p>" + add_form + crossoff_form + page_footer
 
     return content
 
@@ -66,9 +70,13 @@ def add_movie():
     new_movie = request.form['new-movie']
 
     # build response content
-    new_movie_element = "<strong>" + new_movie + "</strong>"
+    if not new_movie:
+        return page_header.format('Please specify the name of the movie you want to add') + add_form + crossoff_form + page_footer
+    new_movie_element = "<strong>" + cgi.escape(new_movie) + "</strong>"
+    if new_movie in terrible_movies:
+        return page_header.format("Trust me, you don't want to add " + new_movie + " to the list") + add_form + crossoff_form + page_footer
     sentence = new_movie_element + " has been added to your Watchlist!"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
+    content = page_header.format('') + "<p>" + sentence + "</p>" + add_form + crossoff_form + page_footer
 
     return content
 
@@ -78,7 +86,7 @@ def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
 
     # build the response string
-    content = page_header + edit_header + add_form + crossoff_form + page_footer
+    content = page_header.format('') + edit_header + add_form + crossoff_form + page_footer
 
     return content
 
